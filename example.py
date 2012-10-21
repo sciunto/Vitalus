ffbb0416194d3021cc416be3678342d0e4ea5ed1
François Boulogne <fboulogne sciunto org>
2012-10-21 16:48:10 +0200

    update example

--- example.py
+++ example.py
@@ -20,25 +20,25 @@ my_backup = vitalus.Vitalus(min_disk_space=0.5)
 my_backup.set_log_level('DEBUG')
 
 # This is my external disk
-target = '/media/disk/backup'
+my_backup.add_destination('/media/disk/backup')
 
 # I add a job for 'my_documents'
 # I want to keep increments (default: False)
-my_backup.add_job('my_documents', '/home/myself/documents', target, incremental=True)
+my_backup.add_job('my_documents', '/home/myself/documents', incremental=True)
 
 # Copy data excepting *.html files.
 # This is a rsync filter rule (man rsync to learn more)
 # filter is a tuple. Don't forget the coma.
-my_backup.add_job('my_data', '/home/myself/data', target, incremental=True, filter=('- *.html',))
+my_backup.add_job('my_data', '/home/myself/data', incremental=True, filter=('- *.html',))
 
 # Another job
 # minimal duration between two backups: 5 hours (default: 24h)
-my_backup.add_job('thunderbird', '/home/myself/.thunderbird', target, period=5, incremental=False)
+my_backup.add_job('thunderbird', '/home/myself/.thunderbird', period=5, incremental=False)
 
 
 # Sync my home space on a server though SSH
 # Keys, without password must be configured
-my_backup.add_job('server', 'myself@server.tld:.', '/home/myself/backup_server')
+my_backup.add_job('server', 'myself@server.tld:.')
 
 
 # Let's go!
