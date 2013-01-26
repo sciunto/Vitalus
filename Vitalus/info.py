e966479c971c9b2103ca1485ac5c37d6f00789d6
François Boulogne <fboulogne sciunto org>
2013-01-26 10:46:08 +0100

    release 0.3.5

--- Vitalus/info.py
+++ Vitalus/info.py
@@ -8,7 +8,7 @@ NAME = "Vitalus"
 URL = "https://github.com/sciunto/Vitalus"
 LICENSE = "GPLv3+"
 EMAIL = "fboulogne@sciunto.org"
-VERSION = "0.3.4"
+VERSION = "0.3.5"
 
 SHORT_DESCRIPTION = "Rsync wrapper for backups"
 
